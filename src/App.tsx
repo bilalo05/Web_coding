bd0d25a03fc478aa34859ec5c311231a2072e944
yahia bilal <yahia.bilal@cnl.dz>
2023-09-21 09:09:55 +0100

    uploade my project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import ProjectsPage from "./projects/ProjectsPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+       <div className="container">
+           <div className="row">
+               <div className="col-sm-12 col-md-4 col-lg-4">
+                   <img src="/assets/logo.png" width={'120px'} alt=""/>
+               </div>
+               <div className="col-sm-12 col-md-4 col-lg-4">
+           <ProjectsPage/>
+               </div>
+               <div className="col-sm-12 col-md-4 col-lg-4 log-btn">
+                   <button className="log">Login</button>
+                   
+               </div>
+           </div>
+       </div>
+     <blockquote cite="Benjamin Franklin">
+           Tell me and I forget, teach me and I may remember, involve me and I learn.
+     </blockquote>
+   
+   </>
   );
 }
 
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,12 @@
+﻿
+const ProjectsPage =() => {
+    
+    return(
+        <>
+            <h1>Projects</h1>
+            
+        </>
+    )
+}
+
+export default ProjectsPage;
