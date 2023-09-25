cbcd6e0bb55516c0888e327aeee3211e3a18f097
yahia bilal <yahia.bilal@cnl.dz>
2023-09-25 16:20:05 +0100

    fix responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ProjectsPage from "./projects/ProjectsPage";
+import Background from "./components/background";
 function App() {
-  return (
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
    <>
        <div className="container">
+
+         
            <div className="row">
-               <div className="col-sm-12 col-md-4 col-lg-4">
+               <div className="col-sm-6 col-md-6 col-lg-6">
                    <img src="/assets/logo.png" width={'120px'} alt=""/>
                </div>
-               <div className="col-sm-12 col-md-4 col-lg-4 text-center">
-                   <h1>Projects</h1>
+               <div className="col-sm-6 col-md-6 col-lg-6 text-center">
+                   <button className="hamburger-menu-button" onClick={toggleMenu}>
+                       <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+                       <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+                       <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+                   </button>
+                   <div className={`menu-items ${isMenuOpen ? 'open' : ''}`} >
+                       <p>Home</p>
+                       <p>Projects</p>
+                       <p>Ideas</p>
+                       <p>Contact</p>
+                   </div>
                </div>
-               <div className="col-sm-12 col-md-4 col-lg-4 log-btn">
+           {/*    <div className="col-sm-12 col-md-4 col-lg-4 log-btn">
                    <button className="log">Login</button>
                    
-               </div>
+               </div>*/}
+           </div>
+           <div className="row">
+               <Background/>
            </div>
        </div>
      <blockquote cite="Benjamin Franklin">
---
+++ src/components/background.tsx
@@ -0,0 +1,84 @@
+﻿import React, { useEffect } from 'react';
+import './background.css';
+import gsap from 'gsap'; // Import GSAP if you haven't already
+
+function Background() {
+    useEffect(() => {
+        // Your script code here
+        document.querySelectorAll('.codedText').forEach((t) => {
+            const arr1 = t.innerHTML.split('');
+            const arr2: string[] = [];
+            arr1.forEach((char, i) => (arr2[i] = randChar())); // Fill arr2 with random characters
+            (t as HTMLElement).onpointerover = () => {
+                const tl = gsap.timeline();
+                let step = 0;
+                tl.fromTo(
+                    t,
+                    {
+                        innerHTML: arr2.join(''),
+                        color: '#000',
+                        background: 'transparent',
+                    },
+                    {
+                        duration: arr1.length / 20, // Duration based on text length
+                        ease: 'power4.in',
+                        delay: 0.1,
+                        color: '#fff',
+                        background: 'transparent',
+                        onUpdate: () => {
+                            const p = Math.floor(tl.progress() * arr1.length); // Whole number from 0 - text length
+                            if (step !== p) {
+                                // Throttle the change of random characters
+                                step = p;
+                                arr1.forEach((char, i) => (arr2[i] = randChar()));
+                                let pt1 = arr1.join('').substring(p, 0),
+                                    pt2 = arr2.join('').substring(arr2.length - p, 0);
+                                if (t.classList.contains('fromRight')) {
+                                    pt1 = arr2.join('').substring(arr2.length - p, 0);
+                                    pt2 = arr1.join('').substring(arr1.length - p);
+                                }
+                                t.innerHTML = pt1 + pt2; // Update text
+                            }
+                        },
+                    }
+                );
+            };
+        });
+    }, []);
+
+    function randChar() {
+        let c = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$^&*()…æ_+-=;[]/~`';
+        c = c[Math.floor(Math.random() * c.length)];
+        return Math.random() > 0.5 ? c : c.toUpperCase();
+    }
+
+    return (
+        <>
+            <main className="app">
+                <div className="boxes">
+                    <div className="intros">
+                        <div className="codedtexts">
+                        <div className="codedText">Hello world</div>
+                        <div className="codedText2">Unlock the potential of professional, clean, and responsive web development with a curated collection <br/> of innovative ideas and code snippets for developers.</div>
+                        </div>
+                   {/*     <div className="codedText">Yahia Bilal</div>
+                        <div className="codedText fromRight">Web Developer</div>*/}
+                        <img className="codingimg" src="/assets/img.svg" alt="" />
+                    </div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </main>
+        </>
+    );
+}
+
+export default Background;
